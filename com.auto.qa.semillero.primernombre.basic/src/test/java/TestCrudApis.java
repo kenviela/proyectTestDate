d5a1d4c9889b0c56313b44be68a32e82493786cc
jesaulopez <111526976+jesaulopez@users.noreply.github.com>
2022-11-02 23:40:56 -0500

    PullRequestReto1

--- com.auto.qa.semillero.primernombre.basic/src/test/java/TestCrudApis.java
+++ com.auto.qa.semillero.primernombre.basic/src/test/java/TestCrudApis.java
@@ -1,50 +1,54 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
 public class TestCrudApis {
-    
+
+    @BeforeEach
+    public void setup(){
+        RestAssured.baseURI="https://reqres.in";
+        RestAssured.basePath="/api";
+        RestAssured.filters(new RequestLoggingFilter(),new ResponseLoggingFilter());
+        RestAssured.requestSpecification = new RequestSpecBuilder().setContentType(ContentType.JSON).build();
+    }
     @Test
     public void PostLoginUnknown(){
-        String response = RestAssured
-                .given()
-                .contentType(ContentType.JSON)
+        given()
                 .body("{\n" +
                         "    \"email\": \"eve.holt@reqres.in\",\n" +
                         "    \"password\": \"pistol\"\n" +
                         "}")
-                .post("https://reqres.in/api/register")
-                .then()
-                .log().all()
-                .extract().asString();
-
-        System.out.println("La respuesta es:\n" + response);
+                .post("login")
+                .then().statusCode(HttpStatus.SC_OK);
     }
-
     @Test
     public void GetListUnknown(){
         RestAssured.when().get("https://reqres.in/api/unknown").
-                then().log().all();
+                then()
+                .body("page",equalTo(1))
+                .body("token",notNullValue());
     }
-
     @Test
     public void PutUpdateUnknown(){
-        RestAssured
-                .given()
+        given()
                 .log().all()
-                .contentType(ContentType.JSON)
                 .body("{\n" +
-                        "    \"name\": \"Marlon Nieves\",\n" +
-                        "    \"job\": \"QA Automation JR\"\n" +
+                        "    \"name\": \"JOSEESAU\",\n" +
+                        "    \"job\": \"Developer\"\n" +
                         "}")
-                .put("https://reqres.in/api/users/7")
-                .then()
-                .log().all()
-                .extract().asString();
+                .put("https://reqres.in/api/users/7");
     }
-
     @Test
     public void DeleteUnknown(){
-        RestAssured.when().delete("https://reqres.in/api/users/2").then().log().all();
+        RestAssured.when().delete("https://reqres.in/api/users/2");
     }
 }
